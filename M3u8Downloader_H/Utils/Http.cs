76cbd58adcdd1d20c5b046ea58d434edafddbab4
Harlan-H <370256750@qq.com>
2022-10-28 10:24:32 +0800

    修改默认的user-agent

--- M3u8Downloader_H/Utils/Http.cs
+++ M3u8Downloader_H/Utils/Http.cs
@@ -28,7 +28,7 @@ namespace M3u8Downloader_H.Utils
             {
                 Timeout = TimeSpan.FromSeconds(10)
             };
-            Client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
+            Client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36");
         }
     }
 }
