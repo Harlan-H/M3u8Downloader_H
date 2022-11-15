5986a09d26731c64c829d3682fc699754e991fcd
Harlan-H <370256750@qq.com>
2022-11-15 19:47:42 +0800

    修复编译报错问题

--- M3u8Downloader_H/Services/PluginService.cs
+++ M3u8Downloader_H/Services/PluginService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using M3u8Downloader_H.Plugin;
 using M3u8Downloader_H.Plugin.PluginClients;
 
