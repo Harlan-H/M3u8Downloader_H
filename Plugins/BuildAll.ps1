790093ce782493201f50f3b3ae53112f721aabb7
Harlan-H <370256750@qq.com>
2022-11-23 21:31:47 +0800

    update BuildAll.ps1

--- Plugins/BuildAll.ps1
+++ Plugins/BuildAll.ps1
@@ -15,13 +15,16 @@ $fileList = Get-ChildItem -Directory Plugins
 foreach($file in $fileList)
 {
     $buildDir = "$file\bin\Publish"
+    Write-Host "buildDir  ${buildDir}"
     $dllpath = "$buildDir\$file.dll"
+    Write-Host "dllpath  ${dllpath}"
     if(Test-Path $dllpath)
     {
         Write-Host "Skipped publish, file already exists."
     }else{
         dotnet publish $PSScriptRoot/$file -o $buildDir -c Release
     }   
+    Write-Host "targetDir/Plugins  ${targetDir/Plugins}"
     Copy-Item $dllpath $targetDir/Plugins
 }
 Write-Host "copied to the ${targetDir}"
