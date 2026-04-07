a6fa4379117eac6a902edec26fd0313950e3bc2c
Harlan-H <370256750@qq.com>
2026-04-07 20:00:53 +0800

    fix: 修复在ubuntu下监听地址不正确的问题

--- M3u8Downloader_H/Converters/PortToLocalAddress.cs
+++ M3u8Downloader_H/Converters/PortToLocalAddress.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Net.Sockets;
 using System.Net;
+using M3u8Downloader_H.Utils;
 
 namespace M3u8Downloader_H.Converters
 {
@@ -10,25 +11,12 @@ namespace M3u8Downloader_H.Converters
         public static PortToLocalAddress Instance { get; } = new PortToLocalAddress();
         public override string Convert(int value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return $"http://{GetLocalIp()}:{value}";
+            return $"http://{LocalIpHelper.GetPreferredLocalIPv4()?.ToString() ?? "0.0.0.0"}:{value}";
         }
 
         public override int ConvertBack(string value, Type targetType, object? parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
-        }
-
-        private static string GetLocalIp()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            return "0.0.0.0";
         }
     }
 }
