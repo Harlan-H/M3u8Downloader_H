a4814572aa9bb3bef2e206dc1fedca7edc3649d4
Harlan-H <370256750@qq.com>
2026-05-13 23:18:15 +0800

    feat: 增加插件的最低版本限制

--- M3u8Downloader_H/ViewModels/Components/PluginOnlineItem.cs
+++ M3u8Downloader_H/ViewModels/Components/PluginOnlineItem.cs
@@ -10,8 +10,9 @@ using System.Threading.Tasks;
 
 namespace M3u8Downloader_H.ViewModels.Components
 {
-    public partial class PluginOnlineItem(Func<CancellationToken,Task> downloadFunc, OnlinePluginManifest onlinePluginManifest, PluginState? pluginState) : ObservableObject
+    public partial class PluginOnlineItem(Func<CancellationToken, Task> downloadFunc, OnlinePluginManifest onlinePluginManifest, PluginState? pluginState) : ObservableObject
     {
+        private readonly bool versionTooLow = Program.Version < onlinePluginManifest.Release.MinAppVersion;
         private bool _installed = false;
         public string Title => onlinePluginManifest.BasicInfo.Title;
         public string Desc => onlinePluginManifest.BasicInfo.Description;
@@ -21,35 +22,34 @@ namespace M3u8Downloader_H.ViewModels.Components
         public DateTime Time =>  onlinePluginManifest.BasicInfo.Time;
 
         [ObservableProperty]
-        public partial LoadState State { get; set; }
+        public partial PluginStatus State { get; set; } = Program.Version < onlinePluginManifest.Release.MinAppVersion ? PluginStatus.VersionTooLow : PluginStatus.Normal;
 
         [ObservableProperty]
-        public partial string ErrorString { get; set; }
+        public partial string ErrorString { get; set; } = string.Empty;
 
-        public bool CanInstallPlugin => _installed  == false && LocalVersion is null;
+        public bool CanInstallPlugin => _installed  == false && !versionTooLow && LocalVersion is null ;
 
         [RelayCommand(CanExecute = nameof(CanInstallPlugin))]
         private async Task InstallPlugin()
         {
             try
             {
-                
                 using CancellationTokenSource cancellationTokenSource = new();
                 cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(30));
-                State = LoadState.Loading;
+                State = PluginStatus.Loading;
                 await downloadFunc.Invoke(cancellationTokenSource.Token);
                 _installed = true;
                 InstallPluginCommand.NotifyCanExecuteChanged();
-                State = LoadState.Loaded;
+                State = PluginStatus.Installed;
             }
             catch (Exception ex) 
             {
-                State = LoadState.Failed;
+                State = PluginStatus.Failed;
                 ErrorString = ex.ToString();
             }
         }
 
-        public bool CanUpdatePlugin => _installed == false && LocalVersion is not null && Version > LocalVersion;
+        public bool CanUpdatePlugin => _installed == false && !versionTooLow && LocalVersion is not null && Version > LocalVersion;
 
         [RelayCommand(CanExecute = nameof(CanUpdatePlugin))]
         private async Task UpdatePlugin()
@@ -58,15 +58,15 @@ namespace M3u8Downloader_H.ViewModels.Components
             {
                 using CancellationTokenSource cancellationTokenSource = new();
                 cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(30));
-                State = LoadState.Loading;
+                State = PluginStatus.Loading;
                 await downloadFunc.Invoke(cancellationTokenSource.Token);
                 _installed = true;
                 UpdatePluginCommand.NotifyCanExecuteChanged();
-                State = LoadState.Loaded;
+                State = PluginStatus.Installed;
             }
             catch (Exception ex)
             {
-                State = LoadState.Failed;
+                State = PluginStatus.Failed;
                 ErrorString = ex.ToString();
             }
         }
